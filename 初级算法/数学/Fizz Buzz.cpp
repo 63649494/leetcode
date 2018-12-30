dadcf71fdd7b356787e899c8ab86ae0beaea4e03
Moete <1165335859@qq.com>
2018-12-30 14:00:13 +0800

    12.30

---
+++ 初级算法/数学/Fizz Buzz.cpp
@@ -0,0 +1,54 @@
+/* 写一个程序，输出从 1 到 n 数字的字符串表示。
+
+1. 如果 n 是3的倍数，输出“Fizz”；
+
+2. 如果 n 是5的倍数，输出“Buzz”；
+
+3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+
+示例：
+
+n = 15,
+
+返回:
+[
+    "1",
+    "2",
+    "Fizz",
+    "4",
+    "Buzz",
+    "Fizz",
+    "7",
+    "8",
+    "Fizz",
+    "Buzz",
+    "11",
+    "Fizz",
+    "13",
+    "14",
+    "FizzBuzz"
+] */
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+		vector<string> a;
+		for(int i=1;i<=n;i++){
+			string str;
+			if(i%3==0){
+				str="Fizz";
+				if(i%5==0)
+					str+="Buzz";
+				a.push_back(str);
+			}
+			else if(i%5==0){
+				str="Buzz";
+				a.push_back(str);
+			}
+			else{
+				str=to_string(i);
+				a.push_back(str);
+			}
+		}
+		return a;
+    }
+};
