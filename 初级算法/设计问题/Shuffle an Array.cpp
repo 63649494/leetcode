2731f8deca20cf31bbae3b26ebee40c07d4bb169
Moete <1165335859@qq.com>
2019-01-01 21:12:18 +0800

    1.1

---
+++ 初级算法/设计问题/Shuffle an Array.cpp
@@ -0,0 +1,47 @@
+/* 打乱一个没有重复元素的数组。
+
+示例:
+
+// 以数字集合 1, 2 和 3 初始化数组。
+int[] nums = {1,2,3};
+Solution solution = new Solution(nums);
+
+// 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。
+solution.shuffle();
+
+// 重设数组到它的初始状态[1,2,3]。
+solution.reset();
+
+// 随机返回数组[1,2,3]打乱后的结果。
+solution.shuffle(); */
+    class Solution {
+        private int[] origin;
+        public Solution(int[] nums) {
+            this.origin = nums;
+        }
+
+        /** Resets the array to its original configuration and return it. */
+        public int[] reset() {
+            return origin;
+        }
+
+        /** Returns a random shuffling of the array. */
+        public int[] shuffle() {
+            Random random = new Random();
+            int[] nums = (int[])origin.clone();
+            for(int i=0;i<origin.length;i++){
+                int seq = random.nextInt(nums.length);
+                int tmp = nums[i];
+                nums[i] = nums[seq];
+                nums[seq] = tmp;
+            }
+            return nums;
+        }
+    }
+
+    /**
+     * Your Solution object will be instantiated and called as such:
+     * Solution obj = new Solution(nums);
+     * int[] param_1 = obj.reset();
+     * int[] param_2 = obj.shuffle();
+     */
